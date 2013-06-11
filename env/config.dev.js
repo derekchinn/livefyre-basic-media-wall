6a342aa4e9cc0010a3a807f15287eb12db41fba7
Derek <derek@livefyre.com>
2013-06-11 16:28:49 -0700

    Removing unnecessary items in configs

--- env/config.dev.js
+++ env/config.dev.js
@@ -1,12 +1,6 @@
 define({
 	network : "ign-int-0.fyre.co",
 	siteId : "304017",
-	ad: {
-		articleId : "IGN_E3_2013_news_feed"
-	},
-	feed: {
-		articleId : "IGN_E3_2013_news_feed"
-	},
 	mediaWall: {
 		articleId : "IGN_E3_2013_media_wall"
 	}
