81586c6f41e54aa6198283175585b2b88b57c3c4
Derek <derek@livefyre.com>
2013-06-11 15:01:38 -0700

    symlinking config file

--- config.js
+++ config.js
@@ -1,13 +1,1 @@
-define({
-	network : "ign-prod.fyre.co",
-	siteId : "334865",
-	ad: {
-		articleId : "IGN_E3_2013_news_feed"
-	},
-	feed: {
-		articleId : "IGN_E3_2013_news_feed"
-	},
-	mediaWall: {
-		articleId : "IGN_E3_2013_media_wall"
-	}
-});
+env/config.prod.js
